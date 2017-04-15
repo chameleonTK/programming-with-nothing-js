15549b100dbd524a089a62ca1c9026a517cd0c33
Pakawat Nakwijit <kramatk@gmail.com>
2017-04-15 12:22:25 +0700

    change all of these functions to nested single-argument function

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
-const zero = (p,x)=> x;
-const one = (p,x)=> p(x);
-const two = (p,x)=> p(p(x));
-const three = (p,x)=> p(p(p(x)));
-
-
-function to_integer(n) {
-    return n((x)=>x+1,0)
-}
-
-
-console.log(to_integer(zero));
-console.log(to_integer(one));
-console.log(to_integer(two));
-console.log(to_integer(three));
+const Zero = (p)=>(x)=>x;
+const ONE = (p)=>(x)=>p(x);
+const TWO = (p)=>(x)=>p(p(x));
+const THREE = (p)=>(x)=>p(p(p(x)));
